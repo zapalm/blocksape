3a6cf6c3386cbf165088858c6d61f51c3993f387
zapalm <zapalm@ya.ru>
2016-08-09 07:41:43 +1000

    // 'ps_versions_compliancy' property was removed

--- blocksape.php
+++ blocksape.php
@@ -26,7 +26,6 @@ class BlockSape extends Module
 		$this->version = '1.2.0';
 		$this->author = 'zapalm';
 		$this->need_instance = 0;
-		$this->ps_versions_compliancy = array('min' => '1.2.0.0', 'max' => '1.6.1.0');
 		$this->bootstrap = false;
 
 		parent::__construct();
